af95dc180101a893a0e7758d54213a3427f71faf
iii <miselrkba@gmail.com>
2020-09-16 08:29:57 +0200

    adding server functionality

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users.js");
+
 const PORT = process.env.PORT || 5000;
 
 const router = require("./router");
@@ -13,11 +15,20 @@ const io = socketio(server);
 io.on("connection", (socket) => {
   console.log("We have a new connection");
 
-  socket.on('join', ({name, room}, callback) => {
-    console.log(name, room);
+  socket.on("join", ({ name, room }, callback) => {
+    const {error, user} = addUser({id: socket.id, name, room});
 
+    if(error) return callback(error);
 
-  })
+    socket.emit('message', {user: 'admin', text: `${user.name}, welcome to the room ${user.room}`})
+
+    socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name}, has joined`})
+
+    socket.join(user.room);
+
+    callback();
+
+  });
 
   socket.on("disconnect", () => {
     console.log("User has left!");
